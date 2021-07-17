372131cf02c942dfab1238a9659856ed94bcf0eb
Nikita Pyanykh <nickkomarowski@gmail.com>
2021-07-17 17:28:13 +0300

    added vue router

---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>Необходимо авторизоваться через
+        <a href="/login">Google</a>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: 'Auth'
+    }
+</script>
+
+<style scoped></style>
