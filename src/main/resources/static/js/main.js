ceb2d8151908fd23c5d9f22122cac96548917cb5
Nikita Pyanykh <nickkomarowski@gmail.com>
2021-07-17 15:25:42 +0300

    added vuex for storing state

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Vuetify from "vuetify";
-
+import '@babel/polyfill'
 import 'api/resource'
 import App from 'pages/App.vue'
+import store from 'store/store'
 import { connect } from "./util/ws";
-
 import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
@@ -15,6 +15,7 @@ Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
+    store,
     render: a => a(App)
 })
 
