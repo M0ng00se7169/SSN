6d94f88ed5d24542d971118db9a19247a1d7164c
Nikita Pyanykh <nickkomarowski@gmail.com>
2021-07-21 18:51:56 +0300

    configured webpack for deploy and made 1st build

--- src/main/resources/js/pages/MessageList.vue
+++ src/main/resources/js/pages/MessageList.vue
@@ -15,7 +15,7 @@
     import {mapGetters} from 'vuex'
     import MessageRow from 'components/messages/MessageRow.vue'
     import MessageForm from 'components/messages/MessageForm.vue'
-    import LazyLoader from '../components/LazyLoader.vue'
+    import LazyLoader from 'components/LazyLoader.vue'
 
     export default {
         components: {
